51daa7aae8fff7914c8106492458de45c909b7f5
Enbo-Lyu <2728358343@qq.com>
2024-06-25 16:43:55 +0100

    update plugin file

--- Plugin.py
+++ Plugin.py
@@ -242,6 +242,191 @@ class CustomReplay_method2(SupervisedPlugin):
             num_workers=num_workers,
             batch_size=strategy.train_mb_size,
             shuffle=shuffle)
+
+
+    def after_training_exp(self, strategy: "SupervisedTemplate", **kwargs):
+        """ We update the buffer after the experience.
+            You can use a different callback to update the buffer in a different place
+        """
+        print("Buffer update.")
+        self.storage_policy.update(strategy, **kwargs)
+
+class CustomReplay_method3(SupervisedPlugin):
+    """
+    method3: fixed replay external data
+    """
+    def __init__(
+        self,
+        # sd_data,
+        mem_size: int = 200,
+        batch_size: Optional[int] = None,
+        batch_size_mem: Optional[int] = None,
+        task_balanced_dataloader: bool = False,
+        storage_policy: Optional["ExemplarsBuffer"] = None,
+        image_folder = None
+        # The policy that controls how to add new exemplars in memory
+                        #
+    ):
+        super().__init__()
+        # self.sd_data = sd_data
+        self.mem_size = mem_size
+        self.batch_size = batch_size
+        self.batch_size_mem = batch_size_mem
+        self.task_balanced_dataloader = task_balanced_dataloader
+
+        self.storage_policy = storage_policy
+        self.image_folder = image_folder
+        assert storage_policy.max_size == self.mem_size
+
+        if storage_policy is not None:  # Use other storage policy
+            self.storage_policy = storage_policy
+            assert storage_policy.max_size == self.mem_size
+        else:  # Default
+            self.storage_policy = ExperienceBalancedBuffer(
+                max_size=self.mem_size, adaptive_size=True
+            )
+
+    def before_training_exp(
+        self,
+        strategy: "SupervisedTemplate",
+        num_workers: int = 0,
+        shuffle: bool = True,
+        drop_last: bool = False,
+        **kwargs
+    ):
+        """
+        Dataloader to build batches containing examples from both memories and
+        the training dataset
+        """
+        if len(self.storage_policy.buffer) == 0:
+            return
+
+#         batch_size = self.batch_size
+#         if batch_size is None:
+#             batch_size = strategy.train_mb_size
+
+        batch_size_mem = self.batch_size_mem
+        if batch_size_mem is None:
+            batch_size_mem = strategy.train_mb_size
+
+        assert strategy.adapted_dataset is not None
+        strategy.dataloader = ReplayDataLoader(
+            strategy.adapted_dataset,
+            self.storage_policy.buffer,
+            batch_size=strategy.train_mb_size,
+            num_workers=num_workers,
+            shuffle=shuffle,
+        )
+
+    def after_training_exp(self, strategy: "SupervisedTemplate", **kwargs):
+        """ We update the buffer after the experience.
+            You can use a different callback to update the buffer in a different place
+        """
+        print("Buffer update.")
+        buffer_size = len(self.storage_policy.buffer)
+        print("buffer size: " + str(buffer_size))
+        num_class = len(self.storage_policy.buffer_datasets)
+        print("current class number in replay buffer: " + str(num_class))
+        # self.storage_policy.update(strategy, **kwargs)
+
+        # print(type(strategy.experience))
+
+        current_experience = strategy.experience
+
+        current_classes = current_experience.classes_in_this_experience
+        print(current_classes)
+        # create the combined txt file with current classes
+        print("start to combine files...")
+        combine_files_with_numbers(
+            self.image_folder,
+            'class',
+            current_classes,
+            self.image_folder + 'combined/')
+        joined_string = '_'.join(str(integer) for integer in current_classes)
+        combined_file_name = self.image_folder + 'combined/' + 'class' + 'combined' + '_' + joined_string + '.txt'
+
+        cur_train_set = [combined_file_name]
+
+        print("start generating sd data scenario...")
+        sd_dataset_scenario =  filelist_benchmark(
+                                None,
+                                train_file_lists = cur_train_set, # train
+                                test_file_lists = [], # test
+                                task_labels = [0],
+                                # complete_test_set_only=True,
+                                train_transform=transform_train,
+                            eval_transform=transform_train
+                            )
+        sd_data = SimpleNamespace(experience = sd_dataset_scenario.train_stream[0])
+        # storage_p.update(strategy_state)
+
+        print("start updating sd data into buffer")
+        self.storage_policy.update(sd_data, **kwargs)
+
+class CustomReplay_method4(SupervisedPlugin):
+    """
+    same with method2
+    """
+    def __init__(
+        self,
+        mem_size: int = 200,
+        batch_size: Optional[int] = None,
+        batch_size_mem: Optional[int] = None,
+        task_balanced_dataloader: bool = False,
+        storage_policy: Optional["ExemplarsBuffer"] = None,
+        # The policy that controls how to add new exemplars in memory
+                        #
+    ):
+        super().__init__()
+        self.mem_size = mem_size
+        self.batch_size = batch_size
+        self.batch_size_mem = batch_size_mem
+        self.task_balanced_dataloader = task_balanced_dataloader
+
+        self.storage_policy = storage_policy
+        assert storage_policy.max_size == self.mem_size
+
+
+
+    def before_training_exp(self,
+                            strategy: "SupervisedTemplate",
+                            num_workers: int = 0,
+                            shuffle: bool = True,
+                            **kwargs):
+        """ Here we set the dataloader. create batch with examples from both
+        training data and external memory"""
+        if len(self.storage_policy.buffer) == 0:
+            return
+
+        # replay dataloader samples mini-batches from the memory and current
+        # data separately and combines them together.
+        print("Override the dataloader.")
+
+        buffer_size = len(self.storage_policy.buffer)
+        print("buffer size: " + str(buffer_size))
+        num_class = len(self.storage_policy.buffer_datasets)
+        print("current class number in replay buffer: " + str(num_class))
+        # for item in self.storage_policy.buffer:
+        # tempory_buffer = AvalancheDataset([])
+
+        tempory_buffer = []
+
+        for i, dataset in enumerate(self.storage_policy.buffer_datasets):
+            sub_data = sample_Avadataset(dataset)
+            if i == 0:
+                tempory_buffer = sub_data
+            else:
+                tempory_buffer = tempory_buffer.concat(sub_data)
+
+
+        # buffer = [sample_Avadataset(dataset) for dataset in self.storage_policy.buffer_datasets]
+        strategy.dataloader = ReplayDataLoader(
+            strategy.adapted_dataset,
+            # self.storage_policy.buffer,
+            tempory_buffer,
+            num_workers=num_workers,
+            batch_size=strategy.train_mb_size,
+            shuffle=shuffle)
         # for x, y, t in dl:
         #     print(t.tolist())
         #     print(len(t.tolist()))
@@ -252,4 +437,83 @@ class CustomReplay_method2(SupervisedPlugin):
         """
         print("Buffer update.")
         self.storage_policy.update(strategy, **kwargs)
+
+
+
+class KNN_storagePlugin(SupervisedPlugin):
+    """
+    KNN plugin
+    """
+
+    def __init__(
+        self,
+        mem_size: int = 200,
+        batch_size: int = None,
+        batch_size_mem: int = None,
+        task_balanced_dataloader: bool = False,
+        storage_policy: Optional["ExemplarsBuffer"] = None,
+    ):
+        super().__init__()
+        self.mem_size = mem_size
+        self.batch_size = batch_size
+        self.batch_size_mem = batch_size_mem
+        self.task_balanced_dataloader = task_balanced_dataloader
+
+        if storage_policy is not None:  # Use other storage policy
+            self.storage_policy = storage_policy
+            assert storage_policy.max_size == self.mem_size
+        else:  # Default
+            self.storage_policy = ExperienceBalancedBuffer(
+                max_size=self.mem_size, adaptive_size=True
+            )
+#         self.accuracy_metric = AccuracyMetric(task='multiclass')
+
+    @property
+    def ext_mem(self):
+        return self.storage_policy.buffer_groups  # a Dict<task_id, Dataset>
+
+    def before_training_exp(
+        self,
+        strategy: "SupervisedTemplate",
+        num_workers: int = 0,
+        shuffle: bool = True,
+        **kwargs
+    ):
+        """
+        Dataloader to build batches containing examples from both memories and
+        the training dataset
+        """
+        if len(self.storage_policy.buffer) == 0:
+            # first experience. We don't use the buffer, no need to change
+            # the dataloader.
+            buffer_size = len(self.storage_policy.buffer)
+            print("buffer size: " + str(buffer_size))
+            return
+
+        batch_size = self.batch_size
+        if batch_size is None:
+            batch_size = strategy.train_mb_size
+
+        batch_size_mem = self.batch_size_mem
+        if batch_size_mem is None:
+            batch_size_mem = strategy.train_mb_size
+
+        strategy.dataloader = ReplayDataLoader(
+            strategy.adapted_dataset,
+            self.storage_policy.buffer,
+            oversample_small_tasks=True,
+            batch_size=batch_size,
+            batch_size_mem=batch_size_mem,
+            task_balanced_dataloader=self.task_balanced_dataloader,
+            num_workers=num_workers,
+            shuffle=shuffle,
+        )
+        buffer_size = len(self.storage_policy.buffer)
+        print("buffer size: " + str(buffer_size))
+
+    def after_training_exp(self, strategy: "SupervisedTemplate", **kwargs):
+        self.storage_policy.update(strategy, **kwargs)
+        buffer_size = len(self.storage_policy.buffer)
+        print("after training exp buffer size: " + str(buffer_size))
+
 
