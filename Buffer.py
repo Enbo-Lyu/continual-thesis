d37db05aff8958243480124ad5b32457b21f49af
Enbo-Lyu <2728358343@qq.com>
2024-06-26 11:21:15 +0100

    update buffer

--- Buffer.py
+++ Buffer.py
@@ -82,3 +82,304 @@ if TYPE_CHECKING:
 # all imports
 
 
+def sample_Avadataset(dataset, percentage= 0.1):
+    """random sample in certain percentage, percentage should be float"""
+    data_len = len(dataset)
+    indices = list(range(data_len))
+    random.shuffle(indices)
+    sample_num = int(data_len * percentage)
+    sampled_indices = random.sample(indices, sample_num)
+
+    new_buffer_data = dataset.subset(sampled_indices)
+
+    return new_buffer_data
+class ParametricBuffer_method1(BalancedExemplarsBuffer):
+    """Stores samples for replay using a custom selection strategy and
+    grouping."""
+
+    def __init__(
+        self,
+        max_size: int,
+        groupby=None,
+        selection_strategy: Optional["ExemplarsSelectionStrategy"] = None,
+    ):
+        """Init.
+        :param max_size: The max capacity of the replay memory.
+        :param groupby: Grouping mechanism. One of {None, 'class', 'task',
+            'experience'}.
+        :param selection_strategy: The strategy used to select exemplars to
+            keep in memory when cutting it off.
+        """
+        super().__init__(max_size)
+        assert groupby in {None, "task", "class", "experience"}, (
+            "Unknown grouping scheme. Must be one of {None, 'task', "
+            "'class', 'experience'}"
+        )
+        self.groupby = groupby
+        ss = selection_strategy or RandomExemplarsSelectionStrategy()
+        self.selection_strategy = ss
+        self.seen_groups: Set[int] = set()
+        self._curr_strategy = None
+
+
+    def update(self, strategy: "SupervisedTemplate", **kwargs):
+        assert strategy.experience is not None
+        new_data: AvalancheDataset = strategy.experience.dataset
+        new_groups = self._make_groups(strategy, new_data)
+        self.seen_groups.update(new_groups.keys())
+
+        # associate lengths to classes
+        lens = self.get_group_lengths(len(self.seen_groups))
+        group_to_len = {}
+        for group_id, ll in zip(self.seen_groups, lens):
+            group_to_len[group_id] = ll
+
+        # update buffers with new data
+        for group_id, new_data_g in new_groups.items():
+            ll = group_to_len[group_id]
+            if group_id in self.buffer_groups:
+                old_buffer_g = self.buffer_groups[group_id]
+                old_buffer_g.update_from_dataset(strategy, new_data_g)
+                old_buffer_g.resize(strategy, ll)
+            else:
+                new_buffer = _ParametricSingleBuffer_method1(ll, self.selection_strategy)
+                new_buffer.update_from_dataset(strategy, new_data_g)
+                self.buffer_groups[group_id] = new_buffer
+
+        # resize buffers
+        for group_id, class_buf in self.buffer_groups.items():
+            self.buffer_groups[group_id].resize(strategy, group_to_len[group_id])
+
+    def _make_groups(
+        self, strategy, data: AvalancheDataset
+    ) -> Dict[int, AvalancheDataset]:
+        """Split the data by group according to `self.groupby`."""
+        # if self.groupby is None:
+        #     return {0: data}
+        # elif self.groupby == "task":
+        #     return self._split_by_task(data)
+        # elif self.groupby == "experience":
+        #     return self._split_by_experience(strategy, data)
+        # elif self.groupby == "class":
+        return self._split_by_class(data)
+        # else:
+        #     assert False, "Invalid groupby key. Should never get here."
+
+    def _split_by_class(self, data: AvalancheDataset) -> Dict[int, AvalancheDataset]:
+        # Get sample idxs per class
+        cl_idxs: Dict[int, List[int]] = defaultdict(list)
+        targets = getattr(data, "targets")
+        for idx, target in enumerate(targets):
+            target = int(target)
+            cl_idxs[target].append(idx)
+
+        # Make AvalancheSubset per class
+        new_groups: Dict[int, AvalancheDataset] = {}
+        for c, c_idxs in cl_idxs.items():
+            new_groups[c] = classification_subset(data, indices=c_idxs)
+        return new_groups
+
+    # def _split_by_experience(
+    #     self, strategy, data: AvalancheDataset
+    # ) -> Dict[int, AvalancheDataset]:
+    #     exp_id = strategy.clock.train_exp_counter + 1
+    #     return {exp_id: data}
+
+    # def _split_by_task(self, data: AvalancheDataset) -> Dict[int, AvalancheDataset]:
+    #     new_groups = {}
+    #     task_set = getattr(data, "task_set")
+    #     for task_id in task_set:
+    #         new_groups[task_id] = task_set[task_id]
+    #     return new_groups
+
+
+
+class _ParametricSingleBuffer_method1(ExemplarsBuffer):
+    """A buffer that stores samples for replay using a custom selection
+    strategy.
+
+    This is a private class. Use `ParametricBalancedBuffer` with
+    `groupby=None` to get the same behavior.
+    """
+
+    def __init__(
+        self,
+        max_size: int,
+        selection_strategy: Optional["ExemplarsSelectionStrategy"] = None,
+    ):
+        """
+        :param max_size: The max capacity of the replay memory.
+        :param selection_strategy: The strategy used to select exemplars to
+                                   keep in memory when cutting it off.
+        """
+        super().__init__(max_size)
+        ss = selection_strategy or RandomExemplarsSelectionStrategy()
+        self.selection_strategy = ss
+        self._curr_strategy = None
+
+    def update(self, strategy: "SupervisedTemplate", **kwargs):
+        assert strategy.experience is not None
+        new_data = strategy.experience.dataset
+        self.update_from_dataset(strategy, new_data)
+
+    def update_from_dataset(self, strategy, new_data):
+        # print(new_data[1])
+        torch.manual_seed(41)
+
+        data_len = len(new_data)
+        indices = torch.randperm(data_len).tolist()
+        random.shuffle(indices)
+        sample_num = int(data_len * 0.1)
+#         sample_num = int(data_len * 0.2)
+        sampled_indices = indices[:sample_num]
+        # new_buffer_data = new_data[:sample_num]
+        # new_buffer_data = Subset(new_data, sampled_indices)
+        new_buffer_data = new_data.subset(sampled_indices)
+
+        self.buffer = self.buffer.concat(new_buffer_data)
+        self.resize(strategy, self.max_size)
+
+    def resize(self, strategy, new_size: int):
+        self.max_size = new_size
+        idxs = self.selection_strategy.make_sorted_indices(
+            strategy=strategy, data=self.buffer
+        )
+        self.buffer = self.buffer.subset(idxs[: self.max_size])
+
+class ParametricBuffer_knn(BalancedExemplarsBuffer):
+    """Stores samples for replay using a custom selection strategy and
+    grouping."""
+
+    def __init__(
+        self,
+        max_size: int,
+        groupby=None,
+        selection_strategy: Optional["ExemplarsSelectionStrategy"] = None,
+    ):
+        """Init.
+
+        :param max_size: The max capacity of the replay memory.
+        :param groupby: Grouping mechanism. One of {None, 'class', 'task',
+            'experience'}.
+        :param selection_strategy: The strategy used to select exemplars to
+            keep in memory when cutting it off.
+        """
+        super().__init__(max_size)
+        assert groupby in {None, "task", "class", "experience"}, (
+            "Unknown grouping scheme. Must be one of {None, 'task', "
+            "'class', 'experience'}"
+        )
+        self.groupby = groupby
+        ss = selection_strategy or RandomExemplarsSelectionStrategy()
+        self.selection_strategy = ss
+        self.seen_groups = set()
+        self._curr_strategy = None
+
+
+    def update(self, strategy: "SupervisedTemplate", **kwargs):
+        new_data = strategy.adapted_dataset
+        new_groups = self._make_groups(strategy, new_data)
+        self.seen_groups.update(new_groups.keys())
+
+        # associate lengths to classes
+        lens = self.get_group_lengths(len(self.seen_groups))
+        group_to_len = {}
+        for group_id, ll in zip(self.seen_groups, lens):
+            group_to_len[group_id] = ll
+
+        # update buffers with new data
+        for group_id, new_data_g in new_groups.items():
+            ll = group_to_len[group_id]
+            if group_id in self.buffer_groups:
+                old_buffer_g = self.buffer_groups[group_id]
+                old_buffer_g.update_from_dataset(strategy, new_data_g)
+                old_buffer_g.resize(strategy, ll)
+            else:
+                new_buffer = _ParametricSingleBuffer_knn(
+                    ll, self.selection_strategy
+                )
+                new_buffer.update_from_dataset(strategy, new_data_g)
+                self.buffer_groups[group_id] = new_buffer
+
+        # resize buffers
+        for group_id, class_buf in self.buffer_groups.items():
+            self.buffer_groups[group_id].resize(
+                strategy, group_to_len[group_id]
+            )
+
+    def _make_groups(self, strategy, data):
+        """Split the data by group according to `self.groupby`."""
+        if self.groupby is None:
+            return {0: data}
+        elif self.groupby == "task":
+            return self._split_by_task(data)
+        elif self.groupby == "experience":
+            return self._split_by_experience(strategy, data)
+        elif self.groupby == "class":
+            return self._split_by_class(data)
+        else:
+            assert False, "Invalid groupby key. Should never get here."
+
+    def _split_by_class(self, data):
+        # Get sample idxs per class
+        class_idxs = {}
+        for idx, target in enumerate(data.targets):
+            if target not in class_idxs:
+                class_idxs[target] = []
+            class_idxs[target].append(idx)
+
+        # Make AvalancheSubset per class
+        new_groups = {}
+        for c, c_idxs in class_idxs.items():
+            new_groups[c] = classification_subset(data, indices=c_idxs)
+        return new_groups
+
+    def _split_by_experience(self, strategy, data):
+        exp_id = strategy.clock.train_exp_counter + 1
+        return {exp_id: data}
+
+    def _split_by_task(self, data):
+        new_groups = {}
+        for task_id in data.task_set:
+            new_groups[task_id] = data.task_set[task_id]
+        return new_groups
+
+class _ParametricSingleBuffer_knn(ExemplarsBuffer):
+    """A buffer that stores samples for replay using a custom selection
+    strategy.
+
+    This is a private class. Use `ParametricBalancedBuffer` with
+    `groupby=None` to get the same behavior.
+    """
+
+    def __init__(
+        self,
+        max_size: int,
+        selection_strategy: Optional["ExemplarsSelectionStrategy"] = None,
+    ):
+        """
+        :param max_size: The max capacity of the replay memory.
+        :param selection_strategy: The strategy used to select exemplars to
+                                   keep in memory when cutting it off.
+        """
+        super().__init__(max_size)
+        ss = selection_strategy or RandomExemplarsSelectionStrategy()
+        self.selection_strategy = ss
+        self._curr_strategy = None
+
+    def update(self, strategy: "SupervisedTemplate", **kwargs):
+        new_data = strategy.adapted_dataset
+        self.update_from_dataset(strategy, new_data)
+
+    def update_from_dataset(self, strategy, new_data):
+        self.buffer = self.buffer.concat(new_data)
+        self.resize(strategy, self.max_size)
+
+    def resize(self, strategy, new_size: int):
+        self.max_size = new_size
+        idxs = self.selection_strategy.make_sorted_indices(
+            strategy=strategy, data=self.buffer
+        )
+        self.buffer = self.buffer.subset(idxs[: self.max_size])
+
+  
